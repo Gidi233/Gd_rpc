c730cfc3e4278f3be1ae7b5f8d1bea59ed62b273
Gidi233 <1989871763@qq.com>
2024-03-29 11:02:43 +0800

    管理epoll，处理channel

---
+++ gdrpc/net/eventLoop.cpp
@@ -0,0 +1,152 @@
+#include "eventLoop.hpp"
+
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/eventfd.h>
+#include <unistd.h>
+
+#include <cstring>
+#include <memory>
+
+#include "../util/log.hpp"
+#include "channel.hpp"
+#include "epoll_poller.hpp"
+namespace gdrpc {
+namespace net {
+// 防止一个线程创建多个EventLoop
+__thread EventLoop* t_loopInThisThread = nullptr;
+
+// 超时时间
+const int kPollTimeMs = 10000;
+
+int createEventfd() {
+  int evtfd = ::eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+  if (evtfd < 0) {
+    char err_msg[BUFSIZ];
+    strerror_r(errno, err_msg, sizeof(err_msg));
+    LOG_FATAL << "eventfd error: " << err_msg;
+  }
+  return evtfd;
+}
+
+EventLoop::EventLoop()
+    : looping_(false),
+      quit_(false),
+      calling_pending_functors_(false),
+      threadId_(CurrentThread::tid()),
+      poller_(new EPollPoller(this)),
+      wakeupFd_(createEventfd()),
+      wakeupChannel_(new Channel(this, wakeupFd_)) {
+  LOG_DEBUG << "EventLoop created " << this << " in thread " << threadId_;
+  if (t_loopInThisThread) {
+    LOG_FATAL << "Another EventLoop " << t_loopInThisThread
+              << " exists in this thread " << threadId_;
+  } else {
+    t_loopInThisThread = this;
+  }
+  wakeupChannel_->setReadCallback(
+      [this](util::Timestamp ts) { handleRead(ts); });
+  wakeupChannel_->enableReading();
+}
+
+EventLoop::~EventLoop() {
+  wakeupChannel_->disableAll();
+  wakeupChannel_->remove();
+  ::close(wakeupFd_);
+  t_loopInThisThread = nullptr;
+}
+
+// 开启事件循环
+void EventLoop::loop() {
+  looping_ = true;
+  quit_ = false;
+
+  LOG_DEBUG << "EventLoop " << this << " start looping.";
+
+  while (!quit_) {
+    active_channels_.clear();
+    epoll_reture_time_ = poller_->poll(kPollTimeMs, active_channels_);
+    for (Channel* channel : active_channels_) {
+      channel->handleEvent(epoll_reture_time_);
+    }
+    doPendingFunctors();
+  }
+  LOG_DEBUG << "EventLoop " << this << " stop looping.";
+  looping_ = false;
+}
+
+void EventLoop::quit() {
+  quit_ = true;
+  if (!isInLoopThread()) {
+    wakeup();
+  }
+}
+
+void EventLoop::runInLoop(Functor cb) {
+  if (isInLoopThread())  // 在当前EventLoop
+  {
+    cb();
+  } else  // 非当前EventLoop
+  {
+    queueInLoop(cb);
+  }
+}
+
+// 把cb放入队列中 唤醒loop所在的线程执行cb
+void EventLoop::queueInLoop(Functor cb) {
+  {
+    std::unique_lock<std::mutex> lock(mutex_);
+    pending_functors_.emplace_back(cb);
+  }
+
+  if (!isInLoopThread() || calling_pending_functors_) {
+    wakeup();
+  }
+}
+
+void EventLoop::handleRead(util::Timestamp ts) {
+  uint64_t one = 1;
+  ssize_t n = read(wakeupFd_, &one, sizeof(one));
+  if (n != sizeof(one)) {
+    LOG_WARN << "EventLoop::handleRead() reads " << n << " bytes instead of 8";
+  }
+}
+
+// epoll读事件用来唤醒loop所在线程
+void EventLoop::wakeup() {
+  uint64_t one = 1;
+  ssize_t n = write(wakeupFd_, &one, sizeof(one));
+  if (n != sizeof(one)) {
+    LOG_WARN << "EventLoop::wakeup() writes " << n << " bytes instead of 8\n";
+  }
+}
+
+void EventLoop::updateChannel(Channel* channel) {
+  poller_->updateChannel(channel);
+}
+
+void EventLoop::removeChannel(Channel* channel) {
+  poller_->removeChannel(channel);
+}
+
+bool EventLoop::hasChannel(Channel* channel) {
+  return poller_->hasChannel(channel);
+}
+
+void EventLoop::doPendingFunctors() {
+  std::vector<Functor> functors;
+  calling_pending_functors_ = true;
+
+  {
+    std::unique_lock<std::mutex> lock(mutex_);
+    functors.swap(pending_functors_);
+  }
+
+  for (const Functor& functor : functors) {
+    functor();
+  }
+
+  calling_pending_functors_ = false;
+}
+}  // namespace net
+}  // namespace gdrpc
---
+++ gdrpc/net/eventLoop.hpp
@@ -0,0 +1,68 @@
+#ifndef _EVENTLOOP_
+#define _EVENTLOOP_
+#include <atomic>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <vector>
+
+#include "../util/current_thread.hpp"
+#include "../util/noncopyable.hpp"
+#include "../util/timestamp.hpp"
+#include "channel.hpp"
+#include "epoll_poller.hpp"
+namespace gdrpc {
+namespace net {
+class Channel;
+class EPollPoller;
+// 事件循环类 主要包含了两个大模块 Channel EPollPoller
+class EventLoop : noncopyable {
+ public:
+  using Functor = std::function<void()>;
+
+  EventLoop();
+  ~EventLoop();
+
+  void loop();
+  void quit();
+
+  util::Timestamp pollReturnTime() const { return epoll_reture_time_; }
+
+  // 在对应loop中执行回调
+  void runInLoop(Functor cb);
+  void queueInLoop(Functor cb);
+
+  // 通过eventfd唤醒loop所在的线程
+  void wakeup();
+
+  void updateChannel(Channel* channel);
+  void removeChannel(Channel* channel);
+  bool hasChannel(Channel* channel);
+
+  // 判断EventLoop对象是否在自己的线程里
+  bool isInLoopThread() const { return threadId_ == CurrentThread::tid(); }
+
+ private:
+  using ChannelList = std::vector<Channel*>;
+  void handleRead(util::Timestamp ts);  // 被其他线程唤醒时读wakeupFd_的8字节
+  void doPendingFunctors();             // 执行上层回调
+
+  std::atomic_bool looping_;
+  std::atomic_bool quit_;
+
+  const pid_t threadId_;  // 标识了当前EventLoop的所属线程id
+
+  util::Timestamp epoll_reture_time_;  // Poller返回发生事件的Channels的时间点
+  std::unique_ptr<EPollPoller> poller_;
+
+  int wakeupFd_;  // 用于被其他线程唤醒
+  std::unique_ptr<Channel> wakeupChannel_;
+
+  ChannelList active_channels_;
+  std::atomic_bool calling_pending_functors_;  // 是否有需要执行的回调操作
+  std::vector<Functor> pending_functors_;  // 存储loop需要执行的所有回调操作
+  std::mutex mutex_;
+};
+}  // namespace net
+}  // namespace gdrpc
+#endif
